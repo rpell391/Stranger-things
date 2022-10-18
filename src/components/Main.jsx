24001cf333e0e8fcb05392e6ddab1c5eb1cbce58
Randy Pellegrin <rpell391@gmail.com>
2022-10-18 10:15:57 -0500

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar"
+const Main = () => {
+  return (
+    <div id="main">
+      <Navbar/>
+      <h1>I am main</h1>
+    </div>
+  );
+};
+
+export default Main;
